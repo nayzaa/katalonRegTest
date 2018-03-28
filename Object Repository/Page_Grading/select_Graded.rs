579bfe630fcf975566f0cd7152b7ffe2d8464e12
nayzaa <nayzaa2010@hotmail.com>
2018-03-29 01:50:07 +0700

    grading test suit

---
+++ Object Repository/Page_Grading/select_Graded.rs
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_Graded</name>
+   <tag></tag>
+   <elementGuidId>2b2386f2-2fee-47db-95d5-5a699c79ef49</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>/html[1]/body[1]/div[@class=&quot;container&quot;]/div[@class=&quot;uk-container&quot;]/div[@class=&quot;uk-container&quot;]/div[@class=&quot;content box&quot;]/div[@class=&quot;uk-overflow-auto&quot;]/table[@class=&quot;uk-table uk-table-justify uk-table-divider uk-table-striped&quot;]/tbody[1]/tr[2]/td[@class=&quot;uk-text-center&quot;]/form[1]/div[@class=&quot;uk-margin&quot;]/div[1]/select[@class=&quot;uk-select&quot;][count(. | //select[@class = 'uk-select']) = count(//select[@class = 'uk-select'])]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>select</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>uk-select</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>onchange</name>
+      <type>Main</type>
+      <value>UIkit.notification({message: 'Set the grade success.', status: 'success', timeout:500})</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+None
+                                                            A
+                                                            B+
+                                                            B
+                                                            C+
+                                                            C
+                                                            D+
+                                                            D
+                                                            F
+                                                        </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[@class=&quot;container&quot;]/div[@class=&quot;uk-container&quot;]/div[@class=&quot;uk-container&quot;]/div[@class=&quot;content box&quot;]/div[@class=&quot;uk-overflow-auto&quot;]/table[@class=&quot;uk-table uk-table-justify uk-table-divider uk-table-striped&quot;]/tbody[1]/tr[2]/td[@class=&quot;uk-text-center&quot;]/form[1]/div[@class=&quot;uk-margin&quot;]/div[1]/select[@class=&quot;uk-select&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Test Suites/STC-11 Set grade.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>STC-11 Set grade</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-29T01:44:34</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c4a2613d-0851-4fe7-b05d-8a5726faf280</testSuiteGuid>
+   <testCaseLink>
+      <guid>f936c278-1030-452c-adb1-36e2de25c37f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Grading_SetA_Success</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0edea039-9d36-400b-9429-705201fc627d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Grading_SetBPlus_Success</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e7d337c8-7a23-441f-8c3c-5c6a3def3eb8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Grading_SetB_Success</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bbe15990-fccb-4d06-9d80-252aa8471765</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Grading_SetCPlus_Success</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d60171b9-1488-4665-9679-b99eea037e42</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Grading_SetC_Success</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>87b9e6df-6498-430d-b7d8-e8e7a367bfd5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Grading_SetDPlus_Success</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0f63a9ce-caad-4312-9ce3-541012249a67</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Grading_SetD_Success</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>face323b-2592-49a8-9aba-3dc9f9984d00</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Grading_SetF_Success</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>aa1979e7-6f26-49ef-90c8-53948487109d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Grading_SetNone_Success</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
