b25c76e8b9f04a11522488f5eb9396701890b439
DESKTOP-Q39DD9V\Max <kanatip_bu@cmu.ac.th>
2018-03-28 22:18:08 +0700

    add stc-04 testcase student see list of course

---
+++ Test Suites/STC-04 See list of course.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>STC-04 See list of course</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>6e78a85f-6860-47c2-87af-4c9e5e057acb</testSuiteGuid>
+</TestSuiteEntity>
