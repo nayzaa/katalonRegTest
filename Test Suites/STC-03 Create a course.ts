7c4183ab7daa1bb8bbc698f55cd88bddbc112cfe
Pannatat Kaeomuen <pannatat_k@cmu.ac.th>
2018-03-29 04:09:14 +0700

    STC-03, STC-08 Test suites

---
+++ Test Suites/STC-03 Create a course.ts
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>STC-03 Create a course</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5b28a901-378b-45de-8dfd-be548bb3dc11</testSuiteGuid>
+   <testCaseLink>
+      <guid>43ecefc4-d332-4c02-97b3-184b8ce805a8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create_Course_AllCorrectInformation_MessageSuccess</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>42e137b2-9d5e-4764-a8f8-216284371eaf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create_Course_BlankAcademicYear_MessagePleaseFill</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1ee3a308-1e05-406b-846b-b4d0577468fd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create_Course_BlankCourseID_MessagePleaseFill</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a7318a57-9f28-483a-8210-cdb131e4c9f1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create_Course_BlankCourseName_MessagePleaseFill</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>254c7fa4-4e54-489b-9b07-56796b04acb4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create_Course_BlankCredit_MessagePleaseFill</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>232275a9-c97b-4c0f-a275-46db6a61bd44</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create_Course_BlankLecturer_MessagePleaseFill</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8f5d1a89-f47c-4920-9955-a336474a9306</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create_Course_BlankMaxStudent_MessagePleaseFill</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4124a567-e228-4620-a595-34a125994797</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create_Course_BlankSection_MessagePleaseFill</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/STC-08 See Tuition fee.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>STC-08 See Tuition fee</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e6a0d5f9-f933-4cd9-84eb-91b1fad948f8</testSuiteGuid>
+   <testCaseLink>
+      <guid>0da11e8c-f9a5-4277-8dfb-2eadbb388756</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/See_Tuition_Fee</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
